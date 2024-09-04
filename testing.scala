1333476f16e05bec90ef5b366731f225ba37eba2
Anudeep-Sanchula <89653210+Anudeep-Sanchula@users.noreply.github.com>
2024-09-04 06:24:54 -0500

    Update testing.scala

--- testing.scala
+++ testing.scala
@@ -1,72 +1,72 @@
 import com.amazonaws.services.glue.GlueContext
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.apache.spark.sql.DataFrame
+import com.amazonaws.services.s3.AmazonS3ClientBuilder
+import com.amazonaws.services.s3.model.{CopyObjectRequest, DeleteObjectRequest}
+
 
 class RvPotentialConditionsGapUpdater {
-  def ReadPipeDelimitedFileFromS3(): Unit = {
-    val sparkConf = new SparkConf().setAppName("ReadPipeDelimitedFileFromS3")
-    val sc: SparkContext = new SparkContext(sparkConf)
-    val glueContext: GlueContext = new GlueContext(sc)
+    def ReadPipeDelimitedFileFromS3(s3Path: String): Unit = {
+        val sparkConf = new SparkConf().setAppName("ReadPipeDelimitedFileFromS3")
+        val sc: SparkContext = new SparkContext(sparkConf)
+        val glueContext: GlueContext = new GlueContext(sc)
+        val df = glueContext.sparkSession.read
+                        .format("csv")
+                        .option("delimiter", "|")
+                        .option("header", "true")
+                        .load(s3Path)
+        
+        val payerLobDf = df.select("PAYER", "LOB")
+        val firstRow = payerLobDf.first()
+        val firstPayer = firstRow.getString(0)
+        val firstLob = firstRow.getString(1)
+        
+        WriteDataFrameToParquet(df, s"s3://testrawbucket123/processed/$firstPayer/$firstLob/")
+  }
+  
 
-    val s3Path = "s3://testrawbucket123/raw/test.csv"
-
-    val df = glueContext.sparkSession.read
-      .format("csv")
-      .option("delimiter", "|")
-      .option("header", "true") 
-      .load(s3Path)
-
-    df.show()
+  def CheckIfEmpty(bucketName: String, sourcePrefix: String, destinationPrefix: String): Unit = {
+      val s3Client = AmazonS3ClientBuilder.defaultClient()
+      val objectListing = s3Client.listObjects(bucketName, sourcePrefix)
+      val objectSummaries = objectListing.getObjectSummaries
+      
+      if (!objectSummaries.isEmpty) {
+          println(s"Found ${objectSummaries.size()} objects to move from '$sourcePrefix' to '$destinationPrefix'.")
+          
+          objectSummaries.forEach { obj =>
+            val oldKey = obj.getKey
+            val newKey = oldKey.replace(sourcePrefix, destinationPrefix)
+            
+            println(s"Moving '$oldKey' to '$newKey'.")
+            
+            s3Client.copyObject(new CopyObjectRequest(bucketName, oldKey, bucketName, newKey))
+            s3Client.deleteObject(new DeleteObjectRequest(bucketName, oldKey))
+          }
+          
+          println("All objects have been moved successfully.")
+          
+      } else {
+          println(s"No objects found under '$sourcePrefix' to move.")
+      }
+  }
+  
+  def WriteDataFrameToParquet(df: DataFrame, outputPath: String): Unit = {
+      CheckIfEmpty("testrawbucket123", "processed", "history")
+      df.write
+          .format("parquet")
+          .mode("overwrite")
+          .save(outputPath)
+      
+      println(s"DataFrame successfully written to Parquet at $outputPath")
   }
 }
 
 object RvPotentialConditionsGapUpdaterJob {
   def main(args: Array[String]): Unit = {
+    raw_s3_path = "s3://testrawbucket123/raw/test.txt"
     val updater = new RvPotentialConditionsGapUpdater()
-    updater.ReadPipeDelimitedFileFromS3()
-  }
-}
-
-#####
-
-import com.amazonaws.services.glue.GlueContext
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
-
-class RvPotentialConditionsGapUpdater {
-
-  def ReadPipeDelimitedFileFromS3(): Unit = {
-    val sparkConf = new SparkConf().setAppName("ReadPipeDelimitedFileFromS3")
-    val sc: SparkContext = new SparkContext(sparkConf)
-    val glueContext: GlueContext = new GlueContext(sc)
-
-    val s3Path = "s3://testrawbucket123/raw/test.csv"
-
-    val df = glueContext.sparkSession.read
-      .format("csv")
-      .option("delimiter", "|")
-      .option("header", "true")
-      .load(s3Path)
-
-    df.show()
-
-    WriteDataFrameToParquet(df, "s3://testrawbucket123/processed/")
-  }
-
-  // Method to write a DataFrame to a Parquet file in S3
-  def WriteDataFrameToParquet(df: org.apache.spark.sql.DataFrame, outputPath: String): Unit = {
-    df.write
-      .format("parquet") // Specify the format as Parquet
-      .mode("overwrite") // Use "overwrite" mode to replace any existing files
-      .save(outputPath) // Specify the output path in S3
-
-    println(s"DataFrame successfully written to Parquet at $outputPath")
-  }
-}
-
-object RvPotentialConditionsGapUpdaterJob {
-  def main(args: Array[String]): Unit = {
-    val updater = new RvPotentialConditionsGapUpdater()
-    updater.ReadPipeDelimitedFileFromS3()
+    
+    updater.ReadPipeDelimitedFileFromS3(raw_s3_path)
   }
 }
